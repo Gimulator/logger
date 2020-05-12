1409bb25fb2c6059c1276030a1d72935cc0a6766
Ali Soume'e <alidadar7676@gmail.com>
2020-05-12 09:49:55 +0430

    Change cmd structure

--- cmd/logger/main.go
+++ cmd/logger/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	client "github.com/Gimulator/client-go"
+	"github.com/Gimulator/logger/concluder"
+	"github.com/Gimulator/logger/recorder"
+	"github.com/Gimulator/logger/uploader"
+)
+
 func main() {
 	runner, err := newRunner()
 	if err != nil {
@@ -9,4 +19,95 @@ func main() {
 	if err := runner.run(); err != nil {
 		panic(err)
 	}
+}
+
+type runner struct {
+	addr     string
+	ch       chan client.Object
+	cli      *client.Client
+	recorder *recorder.Recorder
+
+	uploader  uploader.Uploader
+	concluder concluder.Concluder
+}
+
+func newRunner() (*runner, error) {
+	r := &runner{}
+
+	if err := r.env(); err != nil {
+		return nil, err
+	}
+
+	if err := r.loadClient(); err != nil {
+		return nil, err
+	}
+
+	if err := r.loadUploader(); err != nil {
+		return nil, err
+	}
+
+	if err := r.loadConcluder(); err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func (r *runner) run() error {
+	obj, path := r.recorder.Record()
+
+	if err := r.uploader.Upload(path); err != nil {
+		return err
+	}
+
+	if err := r.concluder.Send(obj); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *runner) loadClient() error {
+	r.cli = client.NewClient(r.addr)
+
+	if err := r.cli.Register(); err != nil {
+		return err
+	}
+
+	r.ch = make(chan client.Object, 1024)
+	if err := r.cli.Socket(r.ch); err != nil {
+		return err
+	}
+
+	if err := r.cli.Watch(client.Key{
+		Type:      "",
+		Name:      "",
+		Namespace: "",
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *runner) loadRecorder() (err error) {
+	r.recorder, err = recorder.NewRecorder(r.ch)
+	return err
+}
+
+func (r *runner) loadUploader() (err error) {
+	r.uploader, err = uploader.NewS3()
+	return err
+}
+
+func (r *runner) loadConcluder() (err error) {
+	r.concluder, err = concluder.NewRabbit()
+	return err
+}
+
+func (r *runner) env() error {
+	r.addr = os.Getenv("LOGGER_ADDRESS")
+	if r.addr == "" {
+		return fmt.Errorf("set the 'LOGGER_ADDRESS' environment variable to connect to Gimulator, like localhost:3030")
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -4,22 +4,18 @@ go 1.14
 
 require (
 	github.com/Gimulator/client-go v0.0.0-20200503001403-e7e9a9b6f3d0
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/minio/minio-go/v6 v6.0.55
-	github.com/mitchellh/mapstructure v1.3.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/pelletier/go-toml v1.7.0 // indirect
-	github.com/spf13/afero v1.2.2 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.6.3
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
-	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79 // indirect
-	golang.org/x/net v0.0.0-20200501053045-e0ff5e5a1de5 // indirect
-	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37 // indirect
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+	golang.org/x/sys v0.0.0-20200511232937-7e40ca221e25 // indirect
 	golang.org/x/text v0.3.2 // indirect
-	gopkg.in/ini.v1 v1.55.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/ini.v1 v1.56.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
